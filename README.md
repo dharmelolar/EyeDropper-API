7e3a199fcf6c27e9c2c012e825281dea0677ec32
Dharmelolar <hardeydoyeen1@gmail.com>
2022-12-06 10:59:45 +0100

    edit readme file

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Getting Started with the EyeDroppper API
 
-The EyeDropper API is a Web API that provides a technique for creating an eyedropper tool. 
-
-This tool allows users to select colors from their screen and also access the value of the color selected. 
+The EyeDropper API is a Web API that provides a technique for creating an eyedropper tool. This tool allows users to select colors from their screen and also access the value of the color selected. 
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// select elements from the dom
+
 const selectBtn = document.getElementById("selectBtn");
 const hexcode = document.getElementById("result");
 const bgColor = document.querySelector("body");
 
+
 selectBtn.addEventListener("click", () => {
+
+  // initialise the eyedropper object
   const eyeDropper = new EyeDropper();
+
+  // start the eyedropper mode 
   eyeDropper.open()
     .then((colorSelectResult) => {
       hexcode.textContent = colorSelectResult.sRGBHex;
